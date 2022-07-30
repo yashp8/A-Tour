ab04e13afe9a7e5b05e394a70c13f755f1cb35ed
mowglie <Yash.Patel@yewtechnologies.co.uk>
2022-07-30 15:35:57 +0100

    save document using mongoose

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //Middle ware
-if (process.env.NODE_ENV == 'development') {
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('hello from middleware.');
--- server.js
+++ server.js
@@ -4,16 +4,53 @@ const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD,
-);
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD,
+// );
 
-mongoose.connect(DB).then(() => {
-  console.log('Database Connected');
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('Database Connected 💾');
+  })
+  .catch(() => {});
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price is required'],
+  },
 });
 
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'test2',
+  price: 2500,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
 const port = process.env.PORT;
-app.listen(port, function () {
+app.listen(port, () => {
   console.log(`server is running on port ${port}`);
 });
